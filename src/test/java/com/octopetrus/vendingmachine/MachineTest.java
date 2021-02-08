db7adee148f6520f085fd96f3d1574bca0b9e19d
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-02-08 14:26:41 +0100

    crate a first Machine class test

---
+++ src/test/java/com/octopetrus/vendingmachine/MachineTest.java
@@ -0,0 +1,37 @@
+package com.octopetrus.vendingmachine;
+
+import com.octopetrus.vendingmachine.machine.Machine;
+import com.octopetrus.vendingmachine.products.Product;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+public class MachineTest {
+
+    private final Product COLA = new Product("Cola", 1.00);
+    private final Product CHIPS = new Product("Chips", 0.50);
+    private final Product CANDY = new Product("Candy", 0.65);
+    private final Product BUBBLE_GUM = new Product("Bubble gum", 0.25);
+
+    private final Map<Product, Integer> productsInMachine = Map.of(
+            COLA, 3,
+            CHIPS, 7,
+            CANDY, 1);
+
+    private final Machine vendingMachine = new Machine(productsInMachine);
+
+    @Test
+    public void addNewProduct_newProduct_productAddToStock() {
+        vendingMachine.addProduct(BUBBLE_GUM, 1);
+
+        Map<Product, Integer> resultOfProductsInMachine = Map.of(
+                COLA, 3,
+                CHIPS, 7,
+                CANDY, 1,
+                BUBBLE_GUM, 1);
+
+        assertEquals(resultOfProductsInMachine, vendingMachine.getProductsInMachine());
+    }
+}
