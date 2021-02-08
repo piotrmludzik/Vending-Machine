ab6f949e8c35f09a4286894821be94276b630fee
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-02-08 12:47:14 +0100

    implement a CoinFactoryTest

---
+++ src/test/java/com/octopetrus/vendingmachine/CoinFactoryTest.java
@@ -0,0 +1,44 @@
+package com.octopetrus.vendingmachine;
+
+import com.octopetrus.vendingmachine.coins.Coin;
+
+import com.octopetrus.vendingmachine.coins.CoinFactory;
+import com.octopetrus.vendingmachine.coins.CoinTypes;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoinFactoryTest {
+
+    private final Coin VALID_COIN = new Coin(5.670, 24.26);  // Quarter dollar
+    private final Coin INVALID_COIN = new Coin(5.660, 24.26);
+
+    private final CoinFactory coinFactory = new CoinFactory();
+
+    @Test
+    public void getCoinName_validCoin_validName() {
+        assertEquals(
+                CoinTypes.QUARTER.name,
+                coinFactory.getName(VALID_COIN));
+    }
+
+    @Test
+    public void getCoinValue_validCoin_validValue() {
+        assertEquals(
+                CoinTypes.QUARTER.value,
+                coinFactory.getValue(VALID_COIN));
+    }
+
+    @Test
+    public void getCoinName_invalidCoin_validName() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> coinFactory.getName(INVALID_COIN));
+    }
+
+    @Test
+    public void getCoinValue_invalidCoin_validValue() {
+        assertThrows(
+                IllegalStateException.class,
+                () -> coinFactory.getValue(INVALID_COIN));
+    }
+}
