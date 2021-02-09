67ab4c5798b4a09b352c775cd47fdad92ebf9608
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-02-09 14:19:22 +0100

    The CoinFactory as singleton

--- src/main/java/com/octopetrus/vendingmachine/coins/CoinFactory.java
+++ src/main/java/com/octopetrus/vendingmachine/coins/CoinFactory.java
@@ -2,7 +2,11 @@ package com.octopetrus.vendingmachine.coins;
 
 public class CoinFactory {
 
-    public Coin createCoin(CoinType coinType) {
+    public static Coin createCoin(CoinType coinType) {
         return new Coin(coinType.weight, coinType.size);
+    }
+
+    private CoinFactory() {
+        throw new AssertionError("The CoinFactory class cannot be an object.");
     }
 }
