7f90a8389f18e3f1a37a180898078d64720d6c95
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-02-09 13:40:36 +0100

    add boundary condition to a Product class constructor

--- src/main/java/com/octopetrus/vendingmachine/products/Product.java
+++ src/main/java/com/octopetrus/vendingmachine/products/Product.java
@@ -6,6 +6,9 @@ public class Product {
     private double price;
 
     public Product(String name, double price) {
+        if (name == null || name.equals("") || price <= 0)
+            throw new IllegalArgumentException("A product should have name and price greater than zero.");
+
         this.name = name;
         this.price = price;
     }
