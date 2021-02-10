145ce4b86b2199feafb1791c27b6ce009e526871
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-02-10 17:30:46 +0100

    remove unnecessary code

--- src/main/java/com/octopetrus/vendingmachine/machine/Machine.java
+++ src/main/java/com/octopetrus/vendingmachine/machine/Machine.java
@@ -10,30 +10,8 @@ public class Machine {
     private final ProductsCtrl productsCtrl = new ProductsCtrl();
     private final MoneyCtrl moneyCtrl = new MoneyCtrl();
 
-    public Machine() {}
-
     public Machine(Map<Product, Integer> products, Map<Coin, Integer> coins) {
         productsCtrl.putProductsIntoMachine(products);
         moneyCtrl.putCoinsIntoMachine(coins);
-    }
-
-    public double getAmountOfTakenCoins() {
-        return moneyCtrl.getAmountOfTakenCoins();
-    }
-
-    public Map<Coin, Integer> getCoinsInMachine() {
-        return moneyCtrl.getCoinsInMachine();
-    }
-
-    public void takeCoin(Coin coin) {
-        moneyCtrl.takeCoin(coin);
-    }
-
-    public Map<Product, Integer> getProductsInMachine() {
-        return productsCtrl.getProductsInMachine();
-    }
-
-    public void takeProduct(Product product) {
-        productsCtrl.takeProduct(product);
     }
 }
